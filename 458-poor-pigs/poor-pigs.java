dc50789cafa4f1946a30a2def159c53c0585b8dd
Harini <harinidpm07@gmail.com>
2026-09-16 00:12:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (16.49%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int rounds = minutesToTest / minutesToDie;
+        int pigs = 0;
+
+        while (Math.pow(rounds + 1, pigs) < buckets) {
+            pigs++;
+        }
+
+        return pigs;
+    }
+}
